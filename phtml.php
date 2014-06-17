8654baa03f4a8e88d0e8d712cda0e0808f00e467
u360 <philipbrucejohnson@gmail.com>
2014-06-17 11:04:58 -0400

    Create phtml.php

---
+++ phtml.php
@@ -0,0 +1,30 @@
+<?php
+/* PHTML 1.0.0 - Super Simple PHP Template Engine
+ * Copyright 2014 Philip Johnson and u360.com
+ * Released under the MIT license
+ * http://github.com/u360/PHTML
+ */
+
+eval( call_user_func( function( $phtml ){
+  $php = array() ;
+  $html = false ;
+  foreach( explode( "\n", $phtml ) as $line ){
+    $data = trim( $line );
+    if( $html || substr( $data, 0, 1 ) == "<" ){
+      $data = str_replace( "\\", "\\\\", $data );
+      $data = str_replace( "'", "\\'", $data );
+      $data = str_replace( "<?", "',", $data );
+      $data = str_replace( "?>", ",'", $data );
+      $php[] = "echo'" . $data . "';" ;
+      if( substr( $data, -1 ) != ">"
+          || $data == "<script>" || $data == "<style>" ){
+        $html = true ;
+      } else {
+        $html = false ;
+      }
+    } else {
+      $php[] = $line ;
+    }
+  }
+  return implode( "\n", $php );
+}, $phtml ) );
