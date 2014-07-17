12af7a5f0df6bb24a8526483a99f1392d094c443
Philip Johnson <philipbrucejohnson@gmail.com>
2014-07-17 16:37:01 -0400

    Update phtml.php

--- phtml.php
+++ phtml.php
@@ -1,23 +1,23 @@
 <?php
 /* PHTML 1.0.0 - Super Simple PHP Template Engine
- * Copyright 2014 Philip Johnson and u360.com
+ * Copyright 2014 Philip Johnson
  * Released under the MIT license
  * http://github.com/u360/PHTML
  */
 
-eval( call_user_func( function( $phtml ){
+eval( call_user_func( function( $phtml ) {
   $php = array() ;
   $html = false ;
-  foreach( explode( "\n", $phtml ) as $line ){
+  foreach( explode( "\n", $phtml ) as $line ) {
     $data = trim( $line );
-    if( $html || substr( $data, 0, 1 ) == "<" ){
+    if( $html || substr( $data, 0, 1 ) == "<" ) {
       $data = str_replace( "\\", "\\\\", $data );
       $data = str_replace( "'", "\\'", $data );
       $data = str_replace( "<?", "',", $data );
       $data = str_replace( "?>", ",'", $data );
       $php[] = "echo'" . $data . "';" ;
       if( substr( $data, -1 ) != ">"
-          || $data == "<script>" || $data == "<style>" ){
+          || $data == "<script>" || $data == "<style>" ) {
         $html = true ;
       } else {
         $html = false ;
